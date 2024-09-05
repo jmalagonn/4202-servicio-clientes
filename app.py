77fe061cbbdc81c111032d6b0136bc867be06bd3
Sergio Perez <se.perezr12@uniandes.edu.co>
2024-09-05 18:47:47 -0500

    Adicion de simulacion de fallo y timeout de servicio

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask_cors import CORS
 from flask_restful import Api
 
 from modelos import db
+from modelos.modelos import Cliente
 from vistas import VistaCliente
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clientes.db'
 app.config['PROPAGATE_EXCEPTIONS'] = True
+
 
 app_context = app.app_context()
 app_context.push()
@@ -19,3 +21,9 @@ cors = CORS(app)
 
 api = Api(app)
 api.add_resource(VistaCliente, '/cliente/<int:id_cliente>')
+nuevo_cliente = Cliente(nombre="ETB")
+nuevo_cliente2 = Cliente(nombre="Movistar")
+
+db.session.add(nuevo_cliente)
+db.session.add(nuevo_cliente2)
+db.session.commit()
--- vistas/vistas.py
+++ vistas/vistas.py
@@ -1,14 +1,31 @@
+from datetime import datetime, timedelta
+import time
+from flask import config
 from flask_restful import Resource
-
 from modelos import db, Cliente, ClienteSchema
 
 cliente_schema = ClienteSchema()
-
+#simulate_error_count = config['simulate_error_count']
+call_counter=0
+last_fail = None
 class VistaCliente(Resource):
+  
   def get(self, id_cliente):
+    global call_counter
+    global last_fail
     cliente = cliente_schema.dump(db.get_or_404(Cliente, id_cliente))
 
-    if cliente is None:
-      return "Cliente no encontrado", 404
-    else: 
-      return {"cliente": cliente}
+    if last_fail is not  None and datetime.now() < last_fail:
+      return "Servicio no disponible", 500
+    else:
+      if call_counter >= 5:
+        print(f"Simulando fallo")
+        last_fail = datetime.now()+timedelta(seconds=5)
+        call_counter = 0
+        return "Servicio no disponible", 500
+      if cliente is None:
+        return "Cliente no encontrado", 404
+      else:
+        call_counter += 1 
+        print(call_counter) 
+        return {"cliente": cliente}
